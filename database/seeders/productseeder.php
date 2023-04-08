dbec5234e9b2675119ae7668f6217051b36e6b90
ziadNady <nadyziad25@gmail.com>
2023-04-08 21:37:33 +0200

    almost finished

---
+++ database/seeders/productseeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
+class productseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i <= 50; $i++) {
+            DB::table('products')->insert([
+                'name' => Str::random(10),
+                'detail' => Str::random(30),
+            ]);
+        }
+    }
+}
